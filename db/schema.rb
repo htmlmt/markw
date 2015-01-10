839415b547a25c8debdf53a802c28db335d1aa2e
Michael Todd <mikejtodd@gmail.com>
2015-01-10 16:30:33 -0600

    Events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150110202656) do
+ActiveRecord::Schema.define(version: 20150110223004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20150110202656) do
     t.string   "address"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "link"
   end
 
   create_table "interviews", force: true do |t|
